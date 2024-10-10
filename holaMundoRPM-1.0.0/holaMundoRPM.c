a1e9d04e887b9ec0ad6a9c5cccd2ba2f9e8749d8
Víctor Emmanuel Rivero Alonzo <nospamvr-git@yahoo.com>
2024-10-09 19:59:58 -0600

    Actualizado holaMundoRPM.c requisitos para GPLv3
    Se actualizo holaMundoRPM-1.0.0/holaMundoRPM.c con el comentario de que
    forma parte del proyecto holaMundoRPM y utiliza la Licencia GPLv3,
    requisito de la licencia.

--- holaMundoRPM-1.0.0/holaMundoRPM.c
+++ holaMundoRPM-1.0.0/holaMundoRPM.c
@@ -1,3 +1,20 @@
+/*
+ * Este archivo es parte de holaMundoRPM.
+ *
+ * holaMundoRPM es software libre: puedes redistribuirlo y/o modificarlo
+ * bajo los términos de la Licencia Pública General de GNU publicada por
+ * la Free Software Foundation, ya sea la versión 3 de la licencia, o
+ * (a tu elección) cualquier versión posterior.
+ *
+ * Este programa se distribuye con la esperanza de que sea útil,
+ * pero SIN NINGUNA GARANTÍA; incluso sin la garantía implícita de
+ * COMERCIALIZACIÓN o ADECUACIÓN A UN PROPÓSITO PARTICULAR. Consulta los detalles en la
+ * Licencia Pública General de GNU.
+ *
+ * Deberías haber recibido una copia de la Licencia Pública General de GNU
+ * junto con este programa. Si no, consulta <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 
 int main(int argc, char *argv[])
